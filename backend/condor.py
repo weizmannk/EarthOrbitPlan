5c1d294808d5d04fd4c1cf57c87312c6a17ad125
Ramodgwendé Weizmann Kiendrébéogo <kiend.weizman7@gmail.com>
2025-04-26 19:49:29 +0200

    Add backend process

---
+++ backend/condor.py
@@ -0,0 +1,38 @@
+import logging
+import subprocess
+
+
+def submit_condor_job(run_name, event_id, args, wrapper_script):
+    condor_submit_script = f"""
+        +MaxHours = 24
+        universe = vanilla
+        accounting_group = ligo.dev.o4.cbc.pe.bayestar
+        envfile = {wrapper_script}
+        getenv = true
+        executable = {wrapper_script}
+        output = {args.log_dir}/$(Cluster)_$(Process).out
+        error = {args.log_dir}/$(Cluster)_$(Process).err
+        log = {args.log_dir}/$(Cluster)_$(Process).log
+        request_memory = 100000 MB
+        request_disk = 10000 MB
+        request_cpus = 1
+        on_exit_remove = (ExitBySignal == False) && (ExitCode == 0)
+        on_exit_hold = (ExitBySignal == True) || (ExitCode != 0)
+        on_exit_hold_reason = (ExitBySignal == True \
+            ? strcat("The job exited with signal ", ExitSignal) \
+            : strcat("The job exited with code ", ExitCode))
+        environment = "OMP_NUM_THREADS=1"
+        queue 1
+        """
+    proc = subprocess.Popen(
+        ["condor_submit"],
+        text=True,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    stdout, stderr = proc.communicate(input=condor_submit_script)
+    if proc.returncode != 0:
+        logging.error(f"Condor submit error for {event_id}: {stderr.strip()}")
+    else:
+        print(f"Submitted condor job for {event_id}: {stdout.strip()}")
---
+++ backend/dask.py
@@ -0,0 +1,53 @@
+import os
+import shlex
+
+from dask_jobqueue import HTCondorCluster
+from distributed import Client, as_completed
+from m4opt._cli import app
+from tqdm import tqdm
+
+
+def run_dask(event_ids, args):
+    """
+    Run M4OPT scheduling tasks using Dask with an HTCondorCluster backend.
+
+    Parameters
+    ----------
+    run_names : list
+        List of run names corresponding to each event ID.
+    event_ids : list
+        List of event IDs to process.
+    args : argparse.Namespace
+        Parsed arguments.
+    """
+
+    def task(run_name, event_id):
+        skymap_file = os.path.join(args.skymap_dir, f"{run_name}/{event_id}.fits")
+        sched_file = os.path.join(args.sched_dir, f"{run_name}/{event_id}.ecsv")
+        cmdline = (
+            f"schedule {skymap_file} {sched_file} --mission={args.mission} --bandpass={args.bandpass} "
+            f"--absmag-mean={args.absmag_mean} --absmag-stdev={args.absmag_stdev} --exptime-min={args.exptime_min}s "
+            f"--exptime-max={args.exptime_max}s --snr={args.snr} --delay='{args.delay}' --deadline='{args.deadline}' "
+            f"--timelimit='{args.timelimit}' --nside={args.nside} --cutoff=0.1 --jobs={args.job_cpu}"
+        )
+        try:
+            app(shlex.split(cmdline))
+        except SystemExit as e:
+            if e.code != 0:
+                raise RuntimeError(
+                    f"Task for event {event_id} failed with exit code {e.code}"
+                )
+
+    cluster = HTCondorCluster(
+        cores=1,
+        memory="50GB",
+        disk="8GB",
+        job_extra={"accounting_group": "ligo.dev.o4.cbc.pe.bayestar"},
+        job_script_prologue=["export OMP_NUM_THREADS=1"],
+    )
+    client = Client(cluster)
+    cluster.adapt(minimum=1, maximum=20)
+
+    futures = client.map(task, event_ids)
+    for future in tqdm(as_completed(futures), total=len(futures)):
+        future.result()
